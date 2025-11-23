66f89e5de4fdaa3fce387109aaf242e566320187
Robikz <zalewapl@gmail.com>
2025-11-23 18:43:29 +0100

    Fix: thumbsticks canceling each other incorrectly
    Events that would arrive for one thumbstick would cause the gizmo to
    move. Then events for the other thumbstick arrived, which could reset
    gizmo's movement even if the original thumbstick was still held.
    
    To prevent that issue and still support use of multiple thumbsticks, sum
    the values of all thumbsticks for the same movement axis when an event
    arrives for one of them.

--- src/controller_handler.cpp
+++ src/controller_handler.cpp
@@ -19,6 +19,9 @@ static const int32_t JOYSTICK_GAMEPAD_RIGHT_THUMBSTICK_VAXIS = 4;
 static const int32_t JOYSTICK_GAMEPAD_LEFT_THROTTLE_AXIS = 2;
 static const int32_t JOYSTICK_GAMEPAD_RIGHT_THROTTLE_AXIS = 5;
 
+static const int32_t JOYSTICK_AXIS_MAX32 = SDL_JOYSTICK_AXIS_MAX;
+static const int32_t JOYSTICK_AXIS_MIN32 = SDL_JOYSTICK_AXIS_MIN;
+
 static const int16_t JOYSTICK_AXIS_THRESHOLD = SDL_JOYSTICK_AXIS_MAX / 10;
 
 static bool is_joystick_horizontal_axis(int32_t axis) {
@@ -34,6 +37,39 @@ static bool is_joystick_vertical_axis(int32_t axis) {
 static bool is_joystick_throttle_axis(int32_t axis) {
 	return axis == JOYSTICK_GAMEPAD_LEFT_THROTTLE_AXIS
 		|| axis == JOYSTICK_GAMEPAD_RIGHT_THROTTLE_AXIS;
+}
+
+static int32_t sum_joystick_thumbsticks_for_axis(SDL_Joystick *joystick, int32_t axis, int32_t deadzone) {
+	SDL_LockJoysticks();
+	const int32_t n_axes = SDL_GetNumJoystickAxes(joystick);
+	std::function<bool(int)> same_axis;
+	if (is_joystick_horizontal_axis(axis)) {
+		same_axis = &is_joystick_horizontal_axis;
+	} else if (is_joystick_vertical_axis(axis)) {
+		same_axis = &is_joystick_vertical_axis;
+	} else {
+		same_axis = [](int axis) { return false; };
+	}
+	int32_t summed_axis_value = 0;
+	for (int32_t n_axis = 0; n_axis < n_axes; ++n_axis) {
+		if (same_axis(n_axis)) {
+			const int32_t current_axis_value = SDL_GetJoystickAxis(joystick, n_axis);
+			if (std::abs(current_axis_value) >= deadzone) {
+				const bool same_sign = (summed_axis_value < 0) == (current_axis_value < 0);
+				if (same_sign) {
+					if (summed_axis_value >= 0) {
+						summed_axis_value = std::min(JOYSTICK_AXIS_MAX32, summed_axis_value + current_axis_value);
+					} else {
+						summed_axis_value = std::max(JOYSTICK_AXIS_MIN32, summed_axis_value + current_axis_value);
+					}
+				} else {
+					summed_axis_value += current_axis_value;
+				}
+			}
+		}
+	}
+	SDL_UnlockJoysticks();
+	return summed_axis_value;
 }
 
 bool JoystickControllerHandler::handle_event(
@@ -54,14 +90,35 @@ bool JoystickControllerHandler::handle_event(
 				state.button_primary = ButtonState::RELEASED;
 			}
 		}
-		const double stick_deadzone = app.settings().joystick_deadzone;
-		if (std::abs(event.jaxis.value) >= stick_deadzone) {
+		const int32_t stick_deadzone = app.settings().joystick_deadzone;
+		// Increase the space to 32-bit for calculations.
+		int32_t axis_value =  event.jaxis.value;
+		// Since multiple thumbsticks can control movement, it's necessary
+		// to sum their values to see if they cancel each other out.
+		auto joystick_it = app.joysticks().find(event.jaxis.which);
+		if (joystick_it != app.joysticks().end()) {
+			axis_value = sum_joystick_thumbsticks_for_axis(
+				joystick_it->second.get(),
+				event.jaxis.axis,
+				stick_deadzone
+			);
+		}
+
+		int32_t axis_value_in_deadzone = 0;
+		int32_t axis_max_in_deadzone = 0;
+		if (axis_value >= 0) {
+			axis_value_in_deadzone = axis_value - stick_deadzone;
+			axis_max_in_deadzone = JOYSTICK_AXIS_MAX32 - stick_deadzone;
+		} else {
+			axis_value_in_deadzone = (axis_value * -1) - stick_deadzone;
+			axis_max_in_deadzone = (JOYSTICK_AXIS_MIN32 * -1) - stick_deadzone;
+		}
+
+		if (axis_value_in_deadzone > 0 && axis_max_in_deadzone > 0) {
 			double normalized = 0.0;
-			if (event.jaxis.value > 0) {
-				normalized = static_cast<double>(event.jaxis.value - stick_deadzone) / (SDL_JOYSTICK_AXIS_MAX - stick_deadzone);
-			} else if (event.jaxis.value < 0) {
-				normalized = static_cast<double>(event.jaxis.value + stick_deadzone) / (SDL_JOYSTICK_AXIS_MIN + stick_deadzone);
-				normalized *= -1.0;
+			normalized = static_cast<double>(axis_value_in_deadzone) / axis_max_in_deadzone;
+			if (axis_value < 0) {
+				normalized *= -1.0f;
 			}
 
 			if (is_joystick_horizontal_axis(axis)) {
